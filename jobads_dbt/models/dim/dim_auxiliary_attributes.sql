5eefed00ed3f204ad7887fddb9a9171105373041
AntonBackeus <antonbackeus@gmail.com>
2024-09-13 16:44:47 +0200

    Added coalesce and capitalize macro to dim files. Also put in occupation_label that was missing

--- jobads_dbt/models/dim/dim_auxiliary_attributes.sql
+++ jobads_dbt/models/dim/dim_auxiliary_attributes.sql
@@ -3,7 +3,7 @@ WITH src_auxiliary_attributes AS (SELECT * FROM {{ ref('src_auxiliary_attributes
 SELECT
     {{ dbt_utils.generate_surrogate_key(['id','experience_required']) }} AS auxiliary_attributes_id, 
     id,
-    experience_required,
-    COALESCE(driver_license, 'not specified') AS driving_license_required,
-    access_to_own_car
+    COALESCE(experience_required, FALSE) AS experience_required,
+    COALESCE(driver_license, FALSE) AS driving_license_required,
+    COALESCE(access_to_own_car, FALSE) AS access_to_own_car
 FROM src_auxiliary_attributes
--- jobads_dbt/models/dim/dim_employer.sql
+++ jobads_dbt/models/dim/dim_employer.sql
@@ -3,12 +3,12 @@ WITH src_employer AS (SELECT * FROM {{ ref('src_employer') }})
 SELECT
     {{ dbt_utils.generate_surrogate_key(['id', 'employer_name']) }} AS employer_id,
     employer_name,
-    COALESCE(employer_workplace, 'workplace not specified') AS employer_workplace,
-    COALESCE(employer_organization_number, 'organization number not specified') AS employer_organization_number,
-    COALESCE(workplace_address, 'street address not specified') AS workplace_street_address,
-    COALESCE(workplace_region, 'region not specified') AS workplace_region,
-    COALESCE(workplace_postcode, 'postcode not specified') AS workplace_postcode,
-    {{ capitalize_first_letter("coalesce(workplace_city, 'city not specified')") }} AS workplace_city,
-    COALESCE(workplace_country, 'country not specified') AS workplace_country
+    COALESCE(employer_workplace, 'Workplace not specified') AS employer_workplace,
+    COALESCE(employer_organization_number, 'Organization number not specified') AS employer_organization_number,
+    {{ capitalize_first_letter("COALESCE(workplace_address, 'Street address not specified')") }} AS workplace_street_address,
+    {{ capitalize_first_letter("COALESCE(workplace_region, 'Region not specified')") }} AS workplace_region,
+    COALESCE(workplace_postcode, 'Postcode not specified') AS workplace_postcode,
+    {{ capitalize_first_letter("coalesce(workplace_city, 'City not specified')") }} AS workplace_city,
+    {{ capitalize_first_letter("COALESCE(workplace_country, 'Country not specified')") }} AS workplace_country
 
 FROM src_employer
