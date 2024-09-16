f06736654f103e86b932fd018e6136bd775a9c76
Emmy Naess <Emmy.naess@live.se>
2024-09-16 14:05:48 +0200

    removed id from auxiliary dim

--- jobads_dbt/models/dim/dim_auxiliary_attributes.sql
+++ jobads_dbt/models/dim/dim_auxiliary_attributes.sql
@@ -1,8 +1,7 @@
 WITH src_auxiliary_attributes AS (SELECT * FROM {{ ref('src_auxiliary_attributes') }})
 
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['id','experience_required']) }} AS auxiliary_attributes_id, 
-    id,
+    {{ dbt_utils.generate_surrogate_key(['id','experience_required']) }} AS auxiliary_attributes_id,
     COALESCE(experience_required, FALSE) AS experience_required,
     COALESCE(driver_license, FALSE) AS driving_license_required,
     COALESCE(access_to_own_car, FALSE) AS access_to_own_car
